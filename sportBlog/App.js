3450c61052c1c069b21fbaa176d406f3f2788479
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2023-11-26 15:27:58 +0200

    save

--- sportBlog/App.js
+++ sportBlog/App.js
@@ -140,44 +140,6 @@ const App = () => {
     );
 
   });
-  {/**
- const [idfa, setIdfa] = useState(null);
-
-  useEffect(() => {
-    ReactNativeIdfaAaid.getAdvertisingInfo()
-      .then((res) =>
-        !res.isAdTrackingLimited ? setIdfa(res.id) : setIdfa(null),
-      )
-      .catch((err) => {
-        console.log(err);
-        return setIdfa(null);
-      });
-  }, []);
-
-  useEffect(() => {
-    if (idfa) {
-      // Метод для запиту дозволів на push-сповіщення
-      OneSignal.Notifications.requestPermission(true);
-    }
-  }, [idfa]);
-  //
-
-  ///////////////код oneSignal
-  
-
-  // Remove this method to stop OneSignal Debugging
-  OneSignal.Debug.setLogLevel(LogLevel.Verbose);
-
-  // OneSignal Initialization
-  OneSignal.initialize("dce89cfe-ba53-4956-8619-e5320af45988");
-
-  // Method for listening for notification clicks
-  OneSignal.Notifications.addEventListener('click', (event) => {
-    console.log('OneSignal: notification clicked:', event);
-  });
-
-  //Add Data Tags
-  OneSignal.User.addTag("key", "value");  */}
 
   ///////////////////////////////////// код лоудера in sportBlog
   const [loaderIsLoaded, setLoaderIsLoaded] = useState(false);
@@ -198,7 +160,7 @@ const App = () => {
     useEffect(() => {
       Animated.timing(appearingAnim, {
         toValue: 1,
-        duration: 5000,
+        duration: 6000,
         useNativeDriver: true,
       }).start();
 
